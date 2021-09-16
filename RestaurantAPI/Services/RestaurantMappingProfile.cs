53bfc5f1577e52ac1cb38a113a4e90fdfbefd1d2
Krzysztof Laskowski <kp.laskowski250@gmail.com>
2021-09-17 01:46:43 +0200

    dto models added

---
+++ RestaurantAPI/Services/RestaurantMappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using RestaurantAPI.Entities;
+using RestaurantAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Services
+{
+    public class RestaurantMappingProfile : Profile
+    {
+        public RestaurantMappingProfile()
+        {
+            CreateMap<Restaurant, RestaurantDto>()
+                .ForMember(m => m.City, c => c.MapFrom(s => s.Address.City))
+                .ForMember(m => m.Street, c => c.MapFrom(s => s.Address.Street))
+                .ForMember(m => m.PostCode, c => c.MapFrom(s => s.Address.PostCode))
+                .ForMember(m => m.Number, c => c.MapFrom(s => s.Address.Number));
+
+            CreateMap<Dish, DishDto>();
+        }
+    }
+}
--- RestaurantAPI/Startup.cs
+++ RestaurantAPI/Startup.cs
@@ -35,6 +35,7 @@ namespace RestaurantAPI
             });
             services.AddDbContext<RestaurantDbContext>();
             services.AddScoped<RestaurantSeeder>();
+            services.AddAutoMapper(this.GetType().Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
