a6b0de2b01c056db03c8acab4bcf038682b50560
Krzysztof Laskowski <kp.laskowski250@gmail.com>
2021-10-13 16:05:38 +0200

    basic user registration action added

---
+++ RestaurantAPI/Services/IAccountService.cs
@@ -0,0 +1,14 @@
+﻿using RestaurantAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Services
+{
+    public interface IAccountService
+    {
+        public void RegisterUser(RegisterUserDto dto);
+    }
+}
