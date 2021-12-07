71363875b1cf3a2a19abe86dae73284bc00c56ef
Krzysztof Laskowski <kp.laskowski250@gmail.com>
2021-12-07 19:16:50 +0100

    test 123

--- RestaurantAPI/Entities/Address.cs
+++ RestaurantAPI/Entities/Address.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace RestaurantAPI.Entities
 {
     public class Address
-    {
+    { 
         public int Id { get; set; }
         public string City { get; set; }
         public string Street { get; set; }
